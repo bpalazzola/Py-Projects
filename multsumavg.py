6703ee8c58d0389db3968b6f68d3a771c0280b4c
bpalazzola <bpalazzola@ymail.com>
2017-10-02 17:32:14 -0500

    type_list

--- multsumavg.py
+++ multsumavg.py
@@ -1,3 +1,5 @@
+for num in range(1, 1000) if range % !=0
+    print(range)
 
 
 a = [1, 2, 5, 10, 255, 3]
@@ -13,4 +15,9 @@ for val in a:
 avg = sum/len(a)
 print (avg)
 
+lower = int(input(0))
+upper = int(input(1000))
+for i in range(lower, upper + 1):
+    if(i % 2 != 0):
+        print(i)
 
---
+++ typelist.py
@@ -0,0 +1,10 @@
+my_list = ['magical unicorns', 19, 'hello', 98.98, 'world']
+
+
+def listType(list):
+    new_string = ""
+    numSum = 0
+    for value in list:
+        print(value)
+
+listType(my_list)
