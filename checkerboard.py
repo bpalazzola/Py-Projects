70c3ac3470a573f4cc55ec61a8215d522e0d8b81
bpalazzola <bpalazzola@ymail.com>
2017-10-03 14:14:57 -0500

    checkerb

--- checkerboard.py
+++ checkerboard.py
@@ -1,14 +1,8 @@
-offset_x = 10      # Distance from left edge.
-offset_y = 10      # Distance from top.
-cell_size = 10     # Height and width of checkerboard squares.
-
-# Note that i ranges from 0 through 7, inclusive.
-for i in range(8):
-    for j in range(8):           # So does j.
-        if (i + j) % 2 == 0:       # The top left square is white.
-            color = 'white'
+def checkerboard():
+    print "running"
+    for i in range(8):
+        if i %2 == 0:
+            print "* * * * "
         else:
-            color = 'black'
-        canvas.setFill(color)
-        canvas.drawRect(offset_x + i * cell_size, offset_y + j * cell_size,
-                        cell_size, cell_size)
+            print " * * * *"
+checkerboard()
