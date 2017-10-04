94ce02fd7e63a8b04866dd72224e7063db38d50c
bpalazzola <bpalazzola@ymail.com>
2017-10-04 16:35:33 -0500

    bike

---
+++ bike.py
@@ -0,0 +1,11 @@
+class Bike:
+    bike1 = Bike(200, "25mph")
+    def __init__(self, price, max_speed, miles)
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = miles
+
+
+#displayInfo() - have this method display the bike's price, maximum speed, and the total miles.
+#ride() - have it display "Riding" on the screen and increase the total miles ridden by 10
+#reverse() - have it display "Reversing" on the screen and decrease the total miles ridden by 5...
