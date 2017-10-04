6c290d9eb50ee9eaff98156e1ace95b39d79ed34
bpalazzola <bpalazzola@ymail.com>
2017-10-04 15:44:28 -0500

    tuples

---
+++ tuplesdic.py
@@ -0,0 +1,9 @@
+my_dict = {
+    {"name" : "Speros", "num" : "(555) 555-5555"},
+    {"name" : "Michael", "num": "(999) 999-9999"},
+    {"name" : "Jay", "num": "(777) 777-7777"}
+#for student in my_dict:
+    #print student["name"] + " " + student["num"]
+
+    tuple_name = ("Speros",)("Michael",)("Jay")
+    tuple_num = ("(555) 555-5555",)("(999) 999-9999",)("(777) 777-7777")
