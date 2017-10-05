438f4e4ef7a8c7437461d21429e32a798067d396
bpalazzola <bpalazzola@ymail.com>
2017-10-04 22:42:58 -0500

    car_redo

--- cars.py
+++ cars.py
@@ -1,14 +1,24 @@
-class Car:
-    def __init__(self, price, speed, mileage, tax)
-        self.price = price
+class Car(object):
+    def __init__(self, price, speed, fuel, mileage):
         self.speed = speed
+        self.fuel = fuel
         self.mileage = mileage
-        self.tax = tax
-    def display_info():
+        self.price = price
+        if price > 10000:
+            self.tax = .15
+        else:
+            self.tax = .12
 
-    def ride():
+    def display_all(self):
+        print 'Price' + str(self.price)
+        print 'Speed' + str(self.speed) + 'mph'
+        print 'Fuel' + self.fuel
+        print 'Mileage' + str(self.mileage) + 'mpg'
+        print 'Tax' + str(self.tax)
 
-    def reverse():
-    
-
-
+car1 = Car(2000, 35, 'Full', 15)
+car2 = Car(2000, 5, 'Not Full', 105)
+car3 = Car(2000, 15, 'Kind of Full', 95)
+car4 = Car(2000, 25, 'Full', 25)
+car5 = Car(2000, 45, 'Empty', 25) 
+car6 = Car(200000000, 35, 'Empty', 15)
